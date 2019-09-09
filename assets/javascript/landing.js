3cc7735d18c435dbf2e2f3fd173575391d315226
sean <spb4578@gmail.com>
2019-09-09 18:33:17 -0400

    new menu, slides for port, about section

--- assets/javascript/landing.js
+++ assets/javascript/landing.js
@@ -1,19 +1,4 @@
 $(document).ready(function () {
-    console.log("ready")
-     // scroll events
-    $(window).on("scroll", function () {
-        if ($(window).scrollTop()) {
-            $("nav").addClass("black")
-            $("nav ul li a").addClass("black")
-            $(".name").addClass("black")
-        }
-
-        else {
-            $("nav").removeClass("black")
-            $("nav ul li a").removeClass("black")
-            $(".name").removeClass("black")
-        }
-    });
 
     $("#goToPort").on("click", function(){
         console.log("go prt working");
